b64d3c089098177f747f28321c5581c95b5bbc6c
César Alvarez Vallero <csralvall@github.com>
2020-04-10 21:30:48 -0300

    Doxygen comments added to all functions and structs

--- single_linked.h
+++ single_linked.h
@@ -40,7 +40,10 @@ typedef node_t* list_t;
  * and the pointer to the next node is set NULL.
  *
  * @param item the integer to be stored
- * @return on succeed, a pointer to the new node, if fails, a NULL pointer
+ * @return on succeed, a pointer to the new node, if fails,
+ * a NULL pointer
+ * @warning if alloc fails, an error message is displayed,
+ and the function returns
  */
 list_t lCreate(int item);
 
@@ -52,7 +55,7 @@ list_t lCreate(int item);
  *
  * @param head pointer to the beginning of the list
  * @return 0 on succeed, 1 on failure
- * @warning if head is NULL, prints error and returns
+ * @note if head is NULL, prints warning and returns
  */
 int lDisplay(list_t head);
 
@@ -66,7 +69,7 @@ int lDisplay(list_t head);
  * @param head pointer to the beginning of the list
  * @param item value of the new node to insert
  * @return 0 on succeed, 1 on failure
- * @warning if head is NULL, prints error and returns
+ * @note if head is NULL, prints warning and returns
  */
 int lInsertT(list_t head, int item);
 
@@ -92,7 +95,8 @@ list_t lInsertH(list_t head, int item);
  * @param item value of the new node to insert
  * @param pos position after which inserts the item
  * @return 0 on succeed, 1 on failure
- * @warning if head is NULL, pos is negative or list is too short, prints error and returns
+ * @warning if head is NULL, pos is negative or list is too short,
+ * prints error and returns
  */
 int lInsertPos(list_t head, int item, int pos);
 
@@ -106,7 +110,8 @@ int lInsertPos(list_t head, int item, int pos);
  * @param item value of the new node to insert
  * @param pre position before which inserts the item
  * @return 0 on succeed, 1 on failure
- * @warning if head is NULL, pre less than one or list is too short, prints error and returns
+ * @warning if head is NULL, pre is less than one or list is too short,
+ * prints error and returns
  */
 int lInsertPre(list_t head, int item, int pre);
 
@@ -117,59 +122,116 @@ int lInsertPre(list_t head, int item, int pre);
  *
  * @param head pointer to the beginning of the list
  * @return pointer to the new list
- * @note if head is NULL, prints a warning message
+ * @note if head is NULL, prints a warning message and returns
  */
 list_t lDeleteH(list_t head);
 
 /**
+ * @brief Deletes the tail of the list.
+ *
+ * Runs over all the list until reachs the last element
+ * and removes it.
  *
  * @param head pointer to the beginning of the list
+ * @return pointer to the list without the last item
+ * @note if head is NULL, prints warning message and returns
  */
 list_t lDeleteT(list_t head);
 
 /**
+ * @brief Deletes the element after a given position.
+ *
+ * Runs over the list until reachs the position given
+ * and deletes the following element, wrapping up the
+ * rest of the list.
  *
  * @param head pointer to the beginning of the list
+ * @param pos position after which removes the element
+ * @return 0 on succeed, 1 on failure
+ * @warning if head is null, pos is negative or greater than list size
+ * the function prints error and returns
  */
 int lDeletePos(list_t head, int pos);
 
 /**
+ * @brief Computes the size of the list.
+ *
+ * Runs over all the list counting the elements.
  *
  * @param head pointer to the beginning of the list
+ * @return the number of elements in the list
+ * @note a null pointer means an empty list
  */
 int lSize(list_t head);
 
 /**
+ * @brief Searchs an item in the list.
+ *
+ * Runs over the list making comparisons until it finds the
+ * element that matchs the key and returns its position.
  *
  * @param head pointer to the beginning of the list
+ * @param key element to search for
+ * @return if finded, the position of the element, -1 on the other case
+ * @note if head is NULL, prints warning message and returns
  */
 int lSearch(list_t head, int key);
 
 /**
+ * @brief Reverse the order of the list.
+ *
+ * Runs over all the list, turning up the order of the nodes.
  *
  * @param head pointer to the beginning of the list
+ * @returns a pointer to the tail of the old list
  */
 list_t lReverse(list_t head);
 
 /**
+ * @brief Takes two list, join them and turns into only one.
  *
+ * Goes to the end of the first list and joins it with the
+ * head of the second list.
+ *
+ * @params a head of the first list
+ * @params b head of the second list
+ * @return a pointer to the beginning of the new list on succeed
+ * or NULL on failure
+ * @warning you can't merge the same list, in case of that
+ prints error and returns
  */
 list_t lMerge(list_t a, list_t b);
 
 /**
+ * @brief Copies all the list on another new list
+ *
+ * Runs over all the list copying on each iteration
+ * the nodes of the original list onto another list.
  *
  * @param head pointer to the beginning of the list
+ * @return a pointer to the new list
  */
 list_t lCopy(list_t head);
 
 /**
+ * @brief Transforms an array into a single linked list
+ * 
+ * Copies all the content of the cells of an array into
+ * nodes and joins all of them on a single linked list.
  *
+ * @param arr pointer to the beginning of the array
+ * @param size size of the array
+ * @return a pointer to the new list on succeed or NULL on failure
  */
 list_t lArrToList(int* arr, int size);
 
 /**
+ * @brief Destroys all the list.
  *
+ * Frees all the nodes in the list.
+
  * @param head pointer to the beginning of the list
+ * @return Void
  */
 void lDestroy(list_t head);
 
