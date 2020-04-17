097818eed68989d0748e8ee4191c2ea411303fff
csralvall <46329881+csralvall@users.noreply.github.com>
2020-04-16 22:44:12 -0300

    all functions redefined to match the new struct implementation

--- inc/single_linked.h
+++ inc/single_linked.h
@@ -18,38 +18,25 @@
 #include <stdlib.h>
 
 /**
- * @brief Structure of each node that belongs to the list.
+ * @brief Defines the list type.
  *
- * Struct that holds an integer and a pointer to the next
- * node of the list.
+ * Redefines the name used to refer to lists. 
  */
-typedef struct node {
-    int data;
-    struct node* link;
-} node_t;
+typedef struct list_t List;
 
 /**
- * @brief Pointer to a node in the list.
+ * @brief Creates a list.
  *
- * Defines a pointer to a node. It's used to point to the head
- * of the list.
- */
-typedef node_t* list_t;
-
-/**
- * @brief Creates a node with value item and returns.
+ * Allocs space for the new list.
+ * If alloc succeeds a new list is created.
  *
- * Allocs space for a new node. If alloc succeeds a new node
- * is created, the value of item is stored inside the node,
- * and the pointer to the next node is set NULL.
- *
- * @param item the integer to be stored
- * @return on succeed, a pointer to the new node, if fails,
- * a NULL pointer
+ * @param Void
+ * @return on success, a pointer to the new list, if fails,
+ * a NULL pointer.
  * @warning if alloc fails, an error message is displayed,
- and the function returns
+ and the function returns.
  */
-list_t create(int item);
+List * init(void);
 
 /**
  * @brief Prints the content of a list.
@@ -57,11 +44,11 @@ list_t create(int item);
  * Runs over the list from beginning to end and
  * prints its content to stdout.
  *
- * @param head pointer to the beginning of the list
- * @return 0 on succeed, 1 on failure
- * @note if head is NULL, prints warning and returns
+ * @param list pointer to the list.
+ * @return 0 on success, 1 on failure.
+ * @note if list is empty, prints error and returns.
  */
-int display(list_t head);
+int display(List *list);
 
 /**
  * @brief Insert an element to the tail of the list.
@@ -70,12 +57,12 @@ int display(list_t head);
  * over all the list and inserts the new element at
  * the end of the list.
  *
- * @param head pointer to the beginning of the list
- * @param item value of the new node to insert
- * @return 0 on succeed, 1 on failure
- * @note if head is NULL, prints warning and returns
+ * @param list pointer to the list.
+ * @param item value of the new node to insert.
+ * @return 0 on success, 1 on failure.
+ * @note if list is empty, prints error and returns.
  */
-int insertT(list_t head, int item);
+int insertT(List *list, int item);
 
 /**
  * @brief Inserts an element at the head of the list.
@@ -83,11 +70,12 @@ int insertT(list_t head, int item);
  * Creates a new node with value equal to item and
  * makes it the new head of the list.
 
- * @param head pointer to the beginning of the list
- * @param item value of the new node to insert
- * @return pointer to the new head of list
+ * @param list pointer to the list.
+ * @param item value of the new node to insert.
+ * @return 0 on success, 1 on failure.
+ * @note if list is empty, prints error and returns.
  */
-list_t insertH(list_t head, int item);
+int insertH(List *list, int item);
 
 /**
  * @brief Inserts an element after a position given.
@@ -95,40 +83,40 @@ list_t insertH(list_t head, int item);
  * Runs over a list until reach the postion given
  * and inserts the element.
  * 
- * @param head pointer to the beginning of the list
- * @param item value of the new node to insert
- * @param pos position after which inserts the item
- * @return 0 on succeed, 1 on failure
- * @warning if head is NULL, pos is negative or list is too short,
- * prints error and returns
+ * @param list pointer to the list.
+ * @param item value of the new node to insert.
+ * @param pos position after which inserts the item.
+ * @return 0 on success, 1 on failure.
+ * @warning if list is empty, pos is negative or list size is less
+ * than pos, prints error and returns.
  */
-int insertPos(list_t head, int item, int pos);
+int insertPos(List *list, int item, int pos);
 
 /**
- * @brief Inserts element befor a position given.
+ * @brief Inserts element before a position given.
  *
  * Runs over a list until reach the element before
  * the position given, and inserts the item after it.
  *
- * @param head pointer to the beginning of the list
- * @param item value of the new node to insert
- * @param pre position before which inserts the item
- * @return 0 on succeed, 1 on failure
- * @warning if head is NULL, pre is less than one or list is too short,
- * prints error and returns
+ * @param list pointer to the list.
+ * @param item value of the new node to insert.
+ * @param pre position before which inserts the item.
+ * @return 0 on success, 1 on failure.
+ * @warning if list is empty, pre is negative or list size is less
+ * or equal to pre, prints error and returns.
  */
-int insertPre(list_t head, int item, int pre);
+int insertPre(List *list, int item, int pre);
 
 /**
  * @brief Deletes the head of the list.
  *
  * Removes the head of the list.
  *
- * @param head pointer to the beginning of the list
- * @return pointer to the new list
- * @note if head is NULL, prints a warning message and returns
+ * @param list pointer to the list.
+ * @return the list without his first element.
+ * @note if list is empty, prints a warning message and returns.
  */
-list_t deleteH(list_t head);
+int deleteH(List *list);
 
 /**
  * @brief Deletes the tail of the list.
@@ -136,11 +124,11 @@ list_t deleteH(list_t head);
  * Runs over all the list until reachs the last element
  * and removes it.
  *
- * @param head pointer to the beginning of the list
- * @return pointer to the list without the last item
- * @note if head is NULL, prints warning message and returns
+ * @param list pointer to the list.
+ * @return the list without the last item.
+ * @note if list is empty, prints warning message and returns.
  */
-list_t deleteT(list_t head);
+int deleteT(List *list);
 
 /**
  * @brief Deletes the element after a given position.
@@ -149,24 +137,33 @@ list_t deleteT(list_t head);
  * and deletes the following element, wrapping up the
  * rest of the list.
  *
- * @param head pointer to the beginning of the list
- * @param pos position after which removes the element
- * @return 0 on succeed, 1 on failure
- * @warning if head is null, pos is negative, equal to zero
- * or greater than list size the function prints error and returns
+ * @param list pointer to the list.
+ * @param pos position after which remove the element.
+ * @return 0 on success, 1 on failure.
+ * @warning if list is empty, pos is negative, equal to zero
+ * or greater than list size the function prints error and returns.
  */
-int deletePos(list_t head, int pos);
+int deletePos(List *list, int pos);
 
 /**
- * @brief Computes the size of the list.
+ * @brief Retrieves the size of the list.
  *
- * Runs over all the list counting the elements.
+ * Returns the number of items inside the list.
  *
- * @param head pointer to the beginning of the list
- * @return the number of elements in the list
- * @note a null pointer means an empty list
+ * @param list pointer to the list.
+ * @return the number of elements inside the list.
  */
-int size(list_t head);
+int size(List *list);
+
+/**
+ * @brief Checks if the list is empty.
+ *
+ * Allows the user to check if the list is empty.
+ *
+ * @param list pointer to the list.
+ * @return 1 if the list is empty, 0 if not.
+ */
+int empty(List *list);
 
 /**
  * @brief Searchs an item in the list.
@@ -174,22 +171,22 @@ int size(list_t head);
  * Runs over the list making comparisons until it finds the
  * element that matchs the key and returns its position.
  *
- * @param head pointer to the beginning of the list
- * @param key element to search for
- * @return if found, the position of the element, -1 on the other case
- * @note if head is NULL, prints warning message and returns
+ * @param list pointer to the list.
+ * @param key element to search for.
+ * @return if found, the position of the element, -1 on the other case.
+ * @note if list is empty, prints error message and returns.
  */
-int search(list_t head, int key);
+int search(List *list, int key);
 
 /**
  * @brief Reverse the order of the list.
  *
  * Runs over all the list, turning up the order of the nodes.
  *
- * @param head pointer to the beginning of the list
- * @returns a pointer to the tail of the old list
+ * @param list pointer to the list.
+ * @return Void
  */
-list_t reverse(list_t head);
+void reverse(List *list);
 
 /**
  * @brief Takes two list, join them and turns into only one.
@@ -197,14 +194,13 @@ list_t reverse(list_t head);
  * Goes to the end of the first list and joins it with the
  * head of the second list.
  *
- * @params a head of the first list
- * @params b head of the second list
- * @return a pointer to the beginning of the new list on succeed
- * or NULL on failure
- * @warning you can't merge the same list, in case of that
- prints error and returns
+ * @params a pointer to the first list.
+ * @params b pointer to the second list.
+ * @return 0 on success, 1 on failure.
+ * @warning in case of null pointer or the same list on both parameters,
+ * prints error and returns.
  */
-list_t merge(list_t a, list_t b);
+int merge(List *a, List *b);
 
 /**
  * @brief Copies all the list on another new list
@@ -212,10 +208,10 @@ list_t merge(list_t a, list_t b);
  * Runs over all the list copying on each iteration
  * the nodes of the original list onto another list.
  *
- * @param head pointer to the beginning of the list
- * @return a pointer to the new list
+ * @param list pointer to the list.
+ * @return a pointer to the new list on success or NULL on failure.
  */
-list_t copy(list_t head);
+List * copy(List *list);
 
 /**
  * @brief Transforms an array into a single linked list
@@ -223,21 +219,22 @@ list_t copy(list_t head);
  * Copies all the content of the cells of an array into
  * nodes and joins all of them on a single linked list.
  *
- * @param arr pointer to the beginning of the array
- * @param size size of the array
- * @return a pointer to the new list on succeed or NULL on failure
+ * @param arr pointer to the beginning of the array.
+ * @param size size of the array.
+ * @return a pointer to the new list on success or NULL on failure.
  */
-list_t arr2list(int* arr, int size);
+List * arr2list(int* arr, int size);
 
 /**
  * @brief Destroys all the list.
  *
- * Frees all the nodes in the list.
-
- * @param head pointer to the beginning of the list
+ * Frees all the nodes in the list and 
+ * frees the structure.
+ *
+ * @param list pointer to the list.
  * @return Void
  */
-void destroy(list_t head);
+void destroy(List *head);
 
 #endif // LIST_H_
 
