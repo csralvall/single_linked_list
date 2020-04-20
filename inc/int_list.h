fa276f728f36cd27f82b3ae42b5e80c4a0885163
csralvall <46329881+csralvall@users.noreply.github.com>
2020-04-19 21:31:51 -0300

    explanation to users added

--- inc/int_list.h
+++ inc/int_list.h
@@ -5,6 +5,15 @@
 
 #define TYPE int
 
+/* The folowing line allows to use
+ * own made names (THING). This also allows
+ * the use of this structure multiple
+ * times inside a project.
+ *
+ * e.g.: TYPED(THING) d_##THING
+ */
+#define TYPED(THING) THING
+
 #include "single_linked.h"
 
 #endif
